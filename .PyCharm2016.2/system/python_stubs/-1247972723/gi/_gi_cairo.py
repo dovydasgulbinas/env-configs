9d71cb0ba23577692e1da686ed92316105c8ceb8
Dovydas Gulbinas <dovyas.gulbinas@gmal.com>
2017-10-28 23:46:53 +0300

    removing useles pycharm settings

--- .PyCharm2016.2/system/python_stubs/-1247972723/gi/_gi_cairo.py
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-# module gi._gi_cairo
-# from /usr/lib/python2.7/dist-packages/gi/_gi_cairo.x86_64-linux-gnu.so
-# by generator 1.143
-# no doc
-# no imports
-
-# no functions
-# no classes
