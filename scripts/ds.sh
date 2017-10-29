d3c306a3e3739bd21aac879036d7e2e905ba4cf1
Dovydas Gulbinas <dovyas.gulbinas@gmal.com>
2017-10-29 20:56:34 +0200

    moving scripts to one place

---
+++ scripts/ds.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Usage:
+#       g query that you would like to search
+#
+
+q=""
+i=0
+
+for var in "$@"
+do
+   if [ "$i" -eq "0" ]
+   then
+      q+=$var
+   else
+      q+="+"$var
+   fi
+   i+=1
+done
+
+open -a safari https://duckduckgo.com/?q=$q
