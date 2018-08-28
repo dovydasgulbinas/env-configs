78ace8e5bfe8af3b94937726a3e51358a66800e8
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2018-08-28 06:05:52 +0300

    Adding proof of concept bookmark-util for newsboat

---
+++ scripts/bookmark-util.sh
@@ -0,0 +1,12 @@
+url="$1"
+title="$2"
+desc="$3"
+
+
+echo "$url"
+echo "$title"
+echo "$desc"
+
+echo "$url" >> test-urls.txt
+
+# https://github.com/newsboat/newsboat/blob/master/contrib/bookmark-delicious.sh
