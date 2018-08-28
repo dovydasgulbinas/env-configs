4ef074c4728f86f788c47d518c0f51b84d682349
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2018-08-28 06:04:15 +0300

    goodbye pusheen

--- scripts/tomcat-start.sh
+++
@@ -1,9 +0,0 @@
-tomcatdir='/Users/hermes/Libs/Tomcat_TEST'
-export JAVA_OPTS="-Djava.security.auth.login.config='$tomcatdir/conf/jaas.conf' -Dorg.apache.el.parser.COERCE_TO_ZERO=false"
-#Start tommy catty
-$tomcatdir/bin/catalina.sh run
-
-#Stop tommy catty
-#/Users/hermes/Libs/Tomcat_TEST/bin/catalina.sh stop
-#ln -s /Users/hermes/Projects/erpx-lithuania/out/artifacts/erpx_richf_1_2_war_exploded test-lt
-
--- scripts/tomcat-stop.sh
+++
@@ -1,3 +0,0 @@
-tomcatdir='/Users/hermes/Libs/Tomcat_TEST'
-#Start tommy catty
-$tomcatdir/bin/catalina.sh stop
