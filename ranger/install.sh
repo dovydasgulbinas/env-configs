7d3dc9dd5525430e37adad731457d02e5dc474e4
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2018-12-11 16:33:22 +0200

    adding more ranger configs

--- ranger/install.sh
+++ ranger/install.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-ln -s "$ENV_CONFIGS_DIR/ranger" "$HOME/.config/ranger"
+make_backup(){
+
+    if [ -f $1 ] || [ -d $1 ] || [ -h $1 ]; then
+        mv $1 "$1.$(date +%s%3N).bak"
+    fi
+}
+
+make_symlink(){
+    ln -s "$1" "$2"
+    ls -alth "$dst"
+}
+
+
+src="$ENV_CONFIG_DIR/ranger"  # place where you keep your confs
+dst="$HOME/.config/ranger"  # e.g. ~/.config/ranger
+
+make_backup $dst
+make_symlink $src $dst
